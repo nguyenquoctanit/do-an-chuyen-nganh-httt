d85fdf4be812e0f202dcda49f2bfc0758c7bc1cf
nguyenquoctanit <nqt.it.bk@gmail.com>
2019-05-01 07:29:00 +0700

    do an chuyen nganh httt

---
+++ shoppc/shoppc/include/footer.php
@@ -0,0 +1,10 @@
+<embed id="Advertisement" 
+width="960" height="100" 
+flashvars="clickTARGET=_blank&clickTAG=#" 
+allowscriptaccess="always"
+wmode="transparent" quality="high" 
+name="Advertisement" style="admincp" 
+src="images/quangcao.swf" 
+type="application/x-shockwave-flash">
+Đồ án Website kinh doanh máy tính xách tay trực tuyến</br>
+Thực hiện: Nguyễn Quốc Tấn- 15T2 - quoctan.it.bkdn@gmail.com - 0354998542
